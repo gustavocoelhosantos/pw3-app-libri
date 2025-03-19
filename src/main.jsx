0fb24221a2d7f5d41e8ebcc2d1083ac4ef4855bd
gustavocoelhosantos <gustavocoelhos.007@gmail.com>
2025-03-19 17:03:31 -0300

    estutura base do projeto pw3-app-libri

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+//arquivo raiz da aplicação onde tudo começa 
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
