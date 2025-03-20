6206a2063fa3f4ed0dea13af368a6ca2213ab4fe
gustavocoelhosantos <gustavocoelhos.007@gmail.com>
2025-03-20 17:07:30 -0300

    estrutura do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
-//arquivo onde irá ser alocado todos os componentes de interfaces gráficas 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+import Container from './components/layout/Container'
+
+/* IMPORTS DOS COMPONENTES DE PÁGINAS */
+import Home from './components/pages/Home'
+import CreateBook from './components/pages/CreateBook'
+import ListBook from './components/pages/ListBook'
+
+/* IMPORTAÇÃO DO NAVBAR */
+import NavBar from './components/layout/NavBar'
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+    
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+        <BrowserRouter>
+
+          <Container>
+
+            <Routes>
+
+              <Route path='/' element={<NavBar />}>
+
+                <Route path='/' element={<Home />} />
+                <Route path='/newBook' element={<CreateBook />} />
+                <Route path='/listBook' element={<ListBook />} />
+
+              </Route>
+
+            </Routes>
+
+          </Container>
+
+        </BrowserRouter>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,3 @@
-//arquivo raiz da aplicação onde tudo começa 
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
